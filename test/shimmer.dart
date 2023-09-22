6b6cf66f2cedcbb800f142cd71c7f49eec189d3f
muthu-gokul <36202759+muthu-gokul@users.noreply.github.com>
2023-09-22 08:00:00 +0530

    Add files via upload
    Shimmer

---
+++ test/shimmer.dart
@@ -0,0 +1,342 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+enum ShimmerDirection { ltr, rtl, ttb, btt }
+
+@immutable
+class Shimmer extends StatefulWidget {
+  final Widget child;
+  final Duration period;
+  final ShimmerDirection direction;
+  final Gradient gradient;
+  final int loop;
+  final bool enabled;
+
+  const Shimmer({
+    Key? key,
+    required this.child,
+    required this.gradient,
+    this.direction = ShimmerDirection.ltr,
+    this.period = const Duration(milliseconds: 1500),
+    this.loop = 0,
+    this.enabled = true,
+  }) : super(key: key);
+
+
+  Shimmer.fromColors({
+    Key? key,
+    required this.child,
+    required Color baseColor,
+    required Color highlightColor,
+    this.period = const Duration(milliseconds: 1500),
+    this.direction = ShimmerDirection.ltr,
+    this.loop = 0,
+    this.enabled = true,
+  })  : gradient = LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.centerRight,
+      colors: <Color>[
+        baseColor,
+        baseColor,
+        highlightColor,
+        baseColor,
+        baseColor
+      ],
+      stops: const <double>[
+        0.0,
+        0.35,
+        0.5,
+        0.65,
+        1.0
+      ]),
+        super(key: key);
+
+  @override
+  _ShimmerState createState() => _ShimmerState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<Gradient>('gradient', gradient,
+        defaultValue: null));
+    properties.add(EnumProperty<ShimmerDirection>('direction', direction));
+    properties.add(
+        DiagnosticsProperty<Duration>('period', period, defaultValue: null));
+    properties
+        .add(DiagnosticsProperty<bool>('enabled', enabled, defaultValue: null));
+    properties.add(DiagnosticsProperty<int>('loop', loop, defaultValue: 0));
+  }
+}
+
+class _ShimmerState extends State<Shimmer> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  int _count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: widget.period)
+      ..addStatusListener((AnimationStatus status) {
+        if (status != AnimationStatus.completed) {
+          return;
+        }
+        _count++;
+        if (widget.loop <= 0) {
+          _controller.repeat();
+        } else if (_count < widget.loop) {
+          _controller.forward(from: 0.0);
+        }
+      });
+    if (widget.enabled) {
+      _controller.forward();
+    }
+  }
+
+  @override
+  void didUpdateWidget(Shimmer oldWidget) {
+    if (widget.enabled) {
+      _controller.forward();
+    } else {
+      _controller.stop();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      child: widget.child,
+      builder: (BuildContext context, Widget? child) => _Shimmer(
+        child: child,
+        direction: widget.direction,
+        gradient: widget.gradient,
+        percent: _controller.value,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+@immutable
+class _Shimmer extends SingleChildRenderObjectWidget {
+  final double percent;
+  final ShimmerDirection direction;
+  final Gradient gradient;
+
+  const _Shimmer({
+    Widget? child,
+    required this.percent,
+    required this.direction,
+    required this.gradient,
+  }) : super(child: child);
+
+  @override
+  _ShimmerFilter createRenderObject(BuildContext context) {
+    return _ShimmerFilter(percent, direction, gradient);
+  }
+
+  @override
+  void updateRenderObject(BuildContext context, _ShimmerFilter shimmer) {
+    shimmer.percent = percent;
+    shimmer.gradient = gradient;
+    shimmer.direction = direction;
+  }
+}
+
+class _ShimmerFilter extends RenderProxyBox {
+  ShimmerDirection _direction;
+  Gradient _gradient;
+  double _percent;
+
+  _ShimmerFilter(this._percent, this._direction, this._gradient);
+
+  @override
+  ShaderMaskLayer? get layer => super.layer as ShaderMaskLayer?;
+
+  @override
+  bool get alwaysNeedsCompositing => child != null;
+
+  set percent(double newValue) {
+    if (newValue == _percent) {
+      return;
+    }
+    _percent = newValue;
+    markNeedsPaint();
+  }
+
+  set gradient(Gradient newValue) {
+    if (newValue == _gradient) {
+      return;
+    }
+    _gradient = newValue;
+    markNeedsPaint();
+  }
+
+  set direction(ShimmerDirection newDirection) {
+    if (newDirection == _direction) {
+      return;
+    }
+    _direction = newDirection;
+    markNeedsLayout();
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    if (child != null) {
+      assert(needsCompositing);
+
+      final double width = child!.size.width;
+      final double height = child!.size.height;
+      Rect rect;
+      double dx, dy;
+      if (_direction == ShimmerDirection.rtl) {
+        dx = _offset(width, -width, _percent);
+        dy = 0.0;
+        rect = Rect.fromLTWH(dx - width, dy, 3 * width, height);
+      } else if (_direction == ShimmerDirection.ttb) {
+        dx = 0.0;
+        dy = _offset(-height, height, _percent);
+        rect = Rect.fromLTWH(dx, dy - height, width, 3 * height);
+      } else if (_direction == ShimmerDirection.btt) {
+        dx = 0.0;
+        dy = _offset(height, -height, _percent);
+        rect = Rect.fromLTWH(dx, dy - height, width, 3 * height);
+      } else {
+        dx = _offset(-width, width, _percent);
+        dy = 0.0;
+        rect = Rect.fromLTWH(dx - width, dy, 3 * width, height);
+      }
+      layer ??= ShaderMaskLayer();
+      layer!
+        ..shader = _gradient.createShader(rect)
+        ..maskRect = offset & size
+        ..blendMode = BlendMode.srcIn;
+      context.pushLayer(layer!, super.paint, offset);
+    } else {
+      layer = null;
+    }
+  }
+
+  double _offset(double start, double end, double percent) {
+    return start + (end - start) * percent;
+  }
+}
+
+
+class BannerPlaceholder extends StatelessWidget {
+  const BannerPlaceholder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 200.0,
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class TitlePlaceholder extends StatelessWidget {
+  final double width;
+
+  const TitlePlaceholder({
+    Key? key,
+    required this.width,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: width,
+            height: 12.0,
+            color: Colors.white,
+          ),
+          const SizedBox(height: 8.0),
+          Container(
+            width: width,
+            height: 12.0,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+enum ContentLineType {
+  twoLines,
+  threeLines,
+}
+
+class ContentPlaceholder extends StatelessWidget {
+  final ContentLineType lineType;
+
+  const ContentPlaceholder({
+    Key? key,
+    required this.lineType,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 96.0,
+            height: 72.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.0),
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(width: 12.0),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 10.0,
+                  color: Colors.white,
+                  margin: const EdgeInsets.only(bottom: 8.0),
+                ),
+                if (lineType == ContentLineType.threeLines)
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 10.0,
+                    color: Colors.white,
+                    margin: const EdgeInsets.only(bottom: 8.0),
+                  ),
+                Container(
+                  width: 100.0,
+                  height: 10.0,
+                  color: Colors.white,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
